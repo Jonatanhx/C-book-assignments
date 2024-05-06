81a9125043145c72abd3d3ea6ed39eb666188861
Jonatan Helander <jonatanhelander@hotmail.com>
2024-05-06 08:23:34 +0200

    exercises 1.8

--- Hello.c
+++ Hello.c
@@ -19,7 +19,6 @@ int main()
 	return 0;
  } ;
 */ 
- #include <stdio.h>
 /* Character Counting */
 /*
 int main() {
@@ -34,7 +33,7 @@ int main() {
 }
 */	
 /*Line counting*/
-int main() {
+/* int main() {
 	int c, nl;
 	
 	nl = 0;
@@ -42,5 +41,34 @@ int main() {
 		if (c == '\n');
 			++nl;
 	printf("%ld\n", nl);
-}	
+}	*/
 
+
+/* 
+Exercise 1-8. Write a program to count blanks, tabs, and newlines.
+Exercise 1-9. Write a program to copy its input to its output, replacing each string of one or
+more blanks by a single blank.
+Exercise 1-10. Write a program to copy its input to its output, replacing each tab by \t, each
+backspace by \b, and each backslash by \\. This makes tabs and backspaces visible in an
+unambiguous way.  
+*/
+int main() {
+	int c, blank, nl, tab;
+	
+	blank = 1;
+	nl = 2;
+	tab = 3;
+	
+	c = 0;
+	
+	while ((c = getchar()) != EOF) {
+		
+		if (c == '\n') /* If a character is new line print 2*/ 
+			printf("%ld\n", nl);
+		else if (c == ' ')
+			printf("%ld\n", blank); /* if character is space print 1 */
+			else if (c == '\t') /* if character is tab print 3*/
+				printf("%d\n", tab);
+	}
+	return 0;
+}
